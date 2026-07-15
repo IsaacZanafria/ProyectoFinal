5cdce399169e90a962aed35f0d64314f0b7edcb0
Jordan Ramos <jordansrm112007@gmail.com>
2026-07-15 00:10:17 -0500

    Añadir menu para los ejercicios

--- main.cpp
+++ main.cpp
@@ -4,12 +4,36 @@
 using namespace std;
 
 int main(){
-	cout << "Ejercicio 1\n====Red Neuronal====\n" << endl;
-	redNeuronal();
-	cout << "\nEjercicio 2\n====Calculadora====\n" << endl;
-	calculadora();
-	cout << "\nEjercicio 3\n====Laberinto====\n" << endl;
-	laberinto();
-	
+	int opcion;
+
+	do{
+    	cout << "===== MENU =====\n";
+    	cout << "1. Red Neuronal\n";
+    	cout << "2. Calculadora\n";
+    	cout << "3. Laberinto\n";
+    	cout << "0. Salir\n";
+    	cout << "Opcion: ";
+    	cin >> opcion;
+
+    	switch(opcion){
+        	case 1:
+        	    redNeuronal();
+        	    break;
+        	case 2:
+        	    calculadora();
+        	    break;
+        	case 3:
+        	    laberinto();
+        	    break;
+        	case 0:
+        		cout<<"------------\n";
+        	    cout << "Hasta luego.\n";
+        	    break;
+        	default:
+        		cout<<"----------------\n";
+            	cout << "Opcion invalida.\n";
+    	}
+
+	}while(opcion != 0);
 	return 0;
 }
